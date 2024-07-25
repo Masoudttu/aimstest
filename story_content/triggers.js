244f812435e2d8760e478fa6b4e5aea8c86cd06c
Masoudttu <164294274+Masoudttu@users.noreply.github.com>
2024-07-25 05:32:18 -0500

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,66 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6VTI81TLFKR":
+        Script1();
+        break;
+      case "6djOBFCoS6R":
+        Script2();
+        break;
+      case "671w1fr0Y1b":
+        Script3();
+        break;
+      case "6Sg9ihCR0eD":
+        Script4();
+        break;
+      case "6ZC76OGgNRq":
+        Script5();
+        break;
+      case "5njkjxFu8xU":
+        Script6();
+        break;
+      case "694MQRZi9Mi":
+        Script7();
+        break;
+      case "5idpecFGAIB":
+        Script8();
+        break;
+      case "5nWFBETQs1u":
+        Script9();
+        break;
+      case "5v9G0xMZtUr":
+        Script10();
+        break;
+      case "5vfJrukjodM":
+        Script11();
+        break;
+      case "6Bwj0Z1LsCE":
+        Script12();
+        break;
+      case "6Dwwp4SiOwM":
+        Script13();
+        break;
+      case "6HSKiJEehkw":
+        Script14();
+        break;
+      case "5w6pf6DaQm9":
+        Script15();
+        break;
+      case "674xLWrfekY":
+        Script16();
+        break;
+      case "6ARbuKA0F7T":
+        Script17();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
